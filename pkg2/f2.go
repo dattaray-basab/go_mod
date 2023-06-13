5389f09689e5a729b3ab51733bb1a958db93234f
Basab Dattaray <91115353+basab-dattaray@users.noreply.github.com>
2023-06-13 09:09:41 -0700

    1.0.4: In Pkg2, G2() calls F2_1()

--- pkg2/f2.go
+++ pkg2/f2.go
@@ -2,6 +2,9 @@ package pkg2
 
 import (
 	"fmt"
+
+	"github.com/dattaray-basab/go_mod/pkg2"
+	"github.com/dattaray-basab/go_mod/pkg2/pkg2-1"
 )
 
 func F2() {
@@ -10,4 +13,5 @@ func F2() {
 
 func G2() {
 	fmt.Println("pkg2.G2()")
+	pkg2.F2_1()
 }
